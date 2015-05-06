adb80da74fb6990dca9f1882d77ff0a55ed55bd4
Alexandre Werneck <xanwerneck@gmail.com>
2015-05-06 14:45:02 -0300

    correct some bugs on script database

---
+++ Trab3/script_create_db.sql
@@ -0,0 +1,10 @@
+-- Database: trab3inf1416
+
+CREATE DATABASE trab3inf1416
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'pt_BR.UTF-8'
+       LC_CTYPE = 'pt_BR.UTF-8'
+       CONNECTION LIMIT = -1;
+
